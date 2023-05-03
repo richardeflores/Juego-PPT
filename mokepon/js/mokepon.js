3d37f4a7ebf26701b859f971a2afb1917a6024c0
Richard Flores <32992340+richardeflores@users.noreply.github.com>
2023-05-02 19:41:36 -0500

    ultimos cambios unificando

--- mokepon/js/mokepon.js
+++
@@ -1,6 +0,0 @@
-function seleccionarMascota(){
-    alert ('SELECCIONASTE TU MASCOTA')
-}
-
-let botonMascota = document.getElementById('boton-mascota')
-botonMascota.addEventListener('click', seleccionarMascota)
