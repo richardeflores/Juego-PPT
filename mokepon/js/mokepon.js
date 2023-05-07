20e6ca5fbe825dcc4fa69d4789390517bd35aee9
Richard Flores <32992340+richardeflores@users.noreply.github.com>
2023-05-07 16:37:39 -0500

    Agregando enemigo aleatorio

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -34,6 +34,36 @@ function seleccionarMascotaJugador(){
     else {
         alert('Debes elegir una mascota')
     }
+    
+    seleccionarMascotaEnemigo ()
+}
+
+function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio = aleatorio(1,6)
+    let spanMascotaEnemigo= document.getElementById('mascota-enemigo')
+
+    if (ataqueAleatorio==1){
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    }
+    else if (ataqueAleatorio==2){
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    }
+    else if (ataqueAleatorio==3){
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+    }
+    else if (ataqueAleatorio==4){
+        spanMascotaEnemigo.innerHTML = 'Langostelvis'
+    }
+    else if (ataqueAleatorio==5){
+        spanMascotaEnemigo.innerHTML = 'Tucapalma'
+    }
+    else {
+        spanMascotaEnemigo.innerHTML = 'Pydos'
+    }
+}
+
+function aleatorio (min, max) {
+    return Math.floor(Math.random()*(max -min +1)+min)
 }
 
 window.addEventListener('load', iniciarJuego)
