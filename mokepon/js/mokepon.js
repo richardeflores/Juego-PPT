ae2ea1864f60433e70ce361b702415aca6895001
Richard Flores <32992340+richardeflores@users.noreply.github.com>
2023-06-19 22:31:38 -0500

    Moviendo a capipepo a la derecha

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -54,6 +54,12 @@ class Mokepon {
         this.foto = foto
         this.vida = vida
         this.ataques = []
+        this.x=20
+        this.y=30
+        this.ancho=80
+        this.alto=80
+        this.mapaFoto=new Image()
+        this.mapaFoto.src=foto
     }
 }
 
@@ -121,9 +127,6 @@ function seleccionarMascotaJugador(){
     sectionSeleccionarMascota.style.display='none'
     sectionVerMapa.style.display='flex'
 
-    let imagenDeCapipepo= new Image()
-    imagenDeCapipepo.src = capipepo.foto
-    lienzo.drawImage(imagenDeCapipepo,20,40,100,100)
 
     if (inputHipodoge.checked){
         spanMascotaJugador.innerHTML = inputHipodoge.id
@@ -299,6 +302,20 @@ function reiniciarJuego() {
 
 function aleatorio (min, max) {
     return Math.floor(Math.random()*(max -min +1)+min)
+}
+
+function pintarPersonaje() {
+    lienzo.clearRect(0,0,mapa.width,mapa.height)
+    lienzo.drawImage(capipepo.mapaFoto,
+        capipepo.x,
+        capipepo.y,
+        capipepo.ancho,
+        capipepo.alto)
+}
+
+function moverCapipepo(){
+        capipepo.x=capipepo.x+5
+        pintarPersonaje()
 }
 
 window.addEventListener('load', iniciarJuego)
