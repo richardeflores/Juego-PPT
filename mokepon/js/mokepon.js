20709df45761d8a81802a89ea69ecbcc10b96e13
Richard Flores <32992340+richardeflores@users.noreply.github.com>
2023-06-28 19:52:18 -0500

    Obstaculos y colisiones

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -332,6 +332,11 @@ function pintarCanvas() {
     hipodogeEnemigo.pintarMokepon()
     capipepoEnemigo.pintarMokepon()
     ratigueyaEnemigo.pintarMokepon()
+    if (mascotaJugadorObjeto.velocidadX!==0 || mascotaJugadorObjeto.velocidadY !==0) {
+        revisarColision(hipodogeEnemigo)
+        revisarColision(capipepoEnemigo)
+        revisarColision(ratigueyaEnemigo)
+    }
 }
 
 function moverDerecha(){
@@ -391,6 +396,36 @@ function obtenerObjetoMascota (){
             return mokepones[i]
         }
     }
+}
+
+function revisarColision(enemigo){
+    const arribaEnemigo=enemigo.y
+    const abajoEnemigo=enemigo.y+enemigo.alto
+    const derechaEnemigo=enemigo.x+enemigo.ancho
+    const izquierdaEnemigo=enemigo.x
+
+    const arribaMascota = 
+        mascotaJugadorObjeto.y
+    const abajoMascota = 
+        mascotaJugadorObjeto.y+mascotaJugadorObjeto.alto
+    const derechaMascota = 
+        mascotaJugadorObjeto.x+mascotaJugadorObjeto.ancho
+    const izquierdaMascota = 
+        mascotaJugadorObjeto.x
+
+
+
+    if (
+        abajoMascota < arribaEnemigo || 
+        arribaMascota > abajoEnemigo || 
+        derechaMascota < izquierdaEnemigo||
+        izquierdaMascota > derechaEnemigo
+    ){
+        return; 
+    }
+    
+    detenerMovimiento()
+    alert('Hay colisión con '+enemigo.nombre)
 }
 
 window.addEventListener('load', iniciarJuego)
