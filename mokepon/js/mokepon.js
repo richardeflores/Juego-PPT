72e3fa10be47e50949093ee1b0c298ffdefb3119
Richard Flores <32992340+richardeflores@users.noreply.github.com>
2023-07-16 22:17:56 -0500

    Optimizando el mapa del juego

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -24,6 +24,7 @@ const mapa = document.getElementById ('mapa')
 
 let jugadorId=null
 let mokepones=[]
+let mokeponesEnemigos=[]
 let ataqueJugador=[]
 let ataqueEnemigo=[]
 let opcionDeMokepones
@@ -367,9 +368,9 @@ function pintarCanvas() {
 
     enviarPosicion(mascotaJugadorObjeto.x, mascotaJugadorObjeto.y)
 
-    hipodogeEnemigo.pintarMokepon()
-    capipepoEnemigo.pintarMokepon()
-    ratigueyaEnemigo.pintarMokepon()
+    mokeponesEnemigos.forEach(function (mokepon){
+        mokepon.pintarMokepon()
+    })
     if (mascotaJugadorObjeto.velocidadX!==0 || mascotaJugadorObjeto.velocidadY !==0) {
         revisarColision(hipodogeEnemigo)
         revisarColision(capipepoEnemigo)
@@ -393,7 +394,7 @@ function enviarPosicion(x,y) {
             res.json()
                 .then(function({enemigos}){
                     console.log(enemigos)
-                    enemigos.forEach(function(enemigo){
+                   mokeponesEnemigos= enemigos.map(function(enemigo){
                         let mokeponEnemigo = null
                         const mokeponNombre = enemigo.mokepon.nombre || ""
                         if (mokeponNombre === "Hipodoge"){
@@ -406,7 +407,7 @@ function enviarPosicion(x,y) {
 
                         mokeponEnemigo.x=enemigo.x
                         mokeponEnemigo.y=enemigo.y
-                        mokeponEnemigo.pintarMokepon()
+                        return mokeponEnemigo
                     })
                 })  
         }
